ae1db341ea79544cf8e2afdcbeaaec18aa03b3a4
parthpsp1 <pspparth1998@gmail.com>
2022-08-17 19:05:54 +0530

    Added Simple Repository Assignment Used DI & Object to access methods

---
+++ Assignment_2_Simple_Repository/SimpleRepositoryPattern/SimpleRepositoryPattern/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SimpleRepositoryPattern.Interface;
+using SimpleRepositoryPattern.Model;
+using SimpleRepositoryPattern.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleRepositoryPattern.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        private readonly EmployeeService employeeService;
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository; // DI through
+            employeeService = new EmployeeService(); // Object through
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            //return await _employeeRepository.Get();
+            return await employeeService.Get();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployees(int id)
+        {
+            return await _employeeRepository.GetById(id);
+        }
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployees(Employee employee)
+        {
+            var AddEmployee = await _employeeRepository.Create(employee);
+            return AddEmployee;
+        }
+    }
+}
---
+++ Assignment_2_Simple_Repository/SimpleRepositoryPattern/SimpleRepositoryPattern/Interface/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using SimpleRepositoryPattern.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleRepositoryPattern.Interface
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> Get();
+        Task<Employee> GetById(int id);
+        Task<Employee> Create(Employee employee);
+        Task Update(Employee employee);
+        //Task Patch(Employee employee);
+        Task Delete(int id);
+    }
+}
